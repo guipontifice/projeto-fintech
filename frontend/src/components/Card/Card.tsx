270f5f34e4a69c3984b2597babf5d3275e62298e
Guilherme <guilhermepontifice.gp@gmail.com>
2025-07-31 20:52:30 -0300

    fix: Card quebrando a aplicação

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import "./Card.css"
 
-interface Props  {
+interface Props {
     companyName: string;
     ticker: string;
     price: number
-    
+
 }
 
-const Card = ({ companyName, ticker, price}: Props) => {
+const Card = ({ companyName, ticker, price }: Props) => {
     return (
         <div className="card">
-            <img
-                src="https://unsplash.com/pt-br/fotografias/as-flores-de-cerejeira-florescem-vibrantemente-contra-um-ceu-azul-N_yJGWotRaE"
-                alt="Imagem aleatória"
-            />
-            <Card companyName="Bradesco"ticker="BR4D" price={48.32} />
-            <Card companyName="Itaú"ticker="IT4U" price={56.32} />
-            <Card companyName="Itausa"ticker="IT4S4" price={32.21} />
             <div className="details">
                 <h2>{companyName}</h2>
                 <p>{ticker}</p>
