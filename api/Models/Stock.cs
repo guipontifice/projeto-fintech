6335dba8f3b8119b677fd979b7fd63a1debc6eae
Guilherme <guilhermepontifice.gp@gmail.com>
2025-08-06 23:21:13 -0300

    feat: adicionando novas props

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,16 +8,21 @@ namespace api.Models
 {
     public class Stock
     {
-     public int Id { get; set;}   
-     
-     public string Symbol { get; set; } = string.Empty;
-     
-     public string CompanyName {get; set;} = string.Empty;
-     [Column(TypeName = "dicemal(18, 2)")]
+        public int Id { get; set; }
 
-     public decimal Purchase { get; set;}
-     [Column(TypeName = "dicemal(18, 2)")]
+        public string Symbol { get; set; } = string.Empty;
 
-public decimal LastDiv { get; set;}
+        public string CompanyName { get; set; } = string.Empty;
+        [Column(TypeName = "dicemal(18, 2)")]
+
+        public decimal Purchase { get; set; }
+        [Column(TypeName = "dicemal(18, 2)")]
+
+        public decimal LastDiv { get; set; }
+        public string Industry { get; set; } = string.Empty;
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
     }
 }
