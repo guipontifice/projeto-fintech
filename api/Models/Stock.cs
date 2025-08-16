968964047ad813cb915b61227166f9016c60b9ff
Guilherme <guilhermepontifice.gp@gmail.com>
2025-08-16 18:21:56 -0300

    fix: corrigindo misspell de dicimal para decimal

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -13,10 +13,10 @@ namespace api.Models
         public string Symbol { get; set; } = string.Empty;
 
         public string CompanyName { get; set; } = string.Empty;
-        [Column(TypeName = "dicemal(18, 2)")]
+        [Column(TypeName = "decimal(18, 2)")]
 
         public decimal Purchase { get; set; }
-        [Column(TypeName = "dicemal(18, 2)")]
+        [Column(TypeName = "decimal(18, 2)")]
 
         public decimal LastDiv { get; set; }
         public string Industry { get; set; } = string.Empty;
